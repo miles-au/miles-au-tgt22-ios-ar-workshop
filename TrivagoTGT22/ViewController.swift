1b5e5bed759a59f523d07e601892e555d3074f03
Miles Au <miles-davin.au@trivago.com>
2022-11-15 18:11:04 +0100

    Remove boilerplate code

--- TrivagoTGT22/ViewController.swift
+++ TrivagoTGT22/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  TrivagoTGT22
-//
-//  Created by Miles Davin Au on 28.10.22.
-//
-
 import UIKit
 import SceneKit
 import ARKit
@@ -44,31 +37,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
